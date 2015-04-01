919c5a540d79e593b47ef258d690bfc5216434d0
m79lol <mikhail@tulkin.name>
2015-04-01 13:50:27 +0500

    Module prepared for float variable values.

--- keyboard_control_module.h
+++ keyboard_control_module.h
@@ -2,28 +2,28 @@
 #define	KEYBOARD_CONTROL_MODULE_H
 
 struct AxisKey {
-	regval axis_index;
-	regval pressed_value;
-	regval unpressed_value;
+	system_value axis_index;
+	variable_value pressed_value;
+	variable_value unpressed_value;
 };
 
 class KeyboardControlModule : public ControlModule {
 	AxisData **robot_axis;
-	int COUNT_AXIS;
+	unsigned int COUNT_AXIS;
 	colorPrintf_t *colorPrintf;
 	
 	bool is_error_init;
 	
-	std::map<std::string, regval> axis_names;
+	std::map<std::string, system_value> axis_names;
 	std::map<WORD, AxisKey*> axis_keys;
-	std::map<regval, AxisData*> axis;
+	std::map<system_value, AxisData*> axis;
 
 	public:
 		KeyboardControlModule();
 		const char *getUID();
 		void prepare(colorPrintf_t *colorPrintf_p, colorPrintfVA_t *colorPrintfVA_p);
 		int init();
-		AxisData** getAxis(int *count_axis);
+		AxisData** getAxis(unsigned int *count_axis);
 		void execute(sendAxisState_t sendAxisState);
 		void final() {};
 		void destroy();
