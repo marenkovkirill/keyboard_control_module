2a8fccce69544e82c46fdf730c7555d0e15ff1e0
m79lol <mikhail@tulkin.name>
2015-07-30 19:49:27 +0500

    Formatted by google style code.

--- keyboard_control_module.h
+++ keyboard_control_module.h
@@ -1,57 +1,58 @@
 #ifndef KEYBOARD_CONTROL_MODULE_H
-#define	KEYBOARD_CONTROL_MODULE_H
+#define KEYBOARD_CONTROL_MODULE_H
 
 struct AxisKey {
-	system_value axis_index;
-	variable_value pressed_value;
-	variable_value unpressed_value;
+  system_value axis_index;
+  variable_value pressed_value;
+  variable_value unpressed_value;
 };
 
 class KeyboardControlModule : public ControlModule {
-	AxisData **robot_axis;
-	unsigned int COUNT_AXIS;
-	colorPrintfModuleVA_t *colorPrintf_p;
-	
-	bool is_error_init;
-	
-	std::map<std::string, system_value> axis_names;
+  AxisData **robot_axis;
+  unsigned int COUNT_AXIS;
+  colorPrintfModuleVA_t *colorPrintf_p;
+
+  bool is_error_init;
+
+  std::map<std::string, system_value> axis_names;
 #ifdef _WIN32
-	std::map<WORD, AxisKey*> axis_keys;
+  std::map<WORD, AxisKey *> axis_keys;
 #else
-	std::map<uint32_t, AxisKey*> axis_keys;
-	std::string InputDevice;
+  std::map<uint32_t, AxisKey *> axis_keys;
+  std::string InputDevice;
 #endif
-	
-	std::map<system_value, AxisData*> axis;
 
-	public:
-		KeyboardControlModule(){};
+  std::map<system_value, AxisData *> axis;
 
-		//init
-		const char *getUID();
-		void prepare(colorPrintfModule_t *colorPrintf_p, colorPrintfModuleVA_t *colorPrintfVA_p);
-	
-		//compiler only
-		AxisData** getAxis(unsigned int *count_axis);
-		void *writePC(unsigned int *buffer_length);
+ public:
+  KeyboardControlModule(){};
 
-		//intepreter - devices
-		int init();
-		void execute(sendAxisState_t sendAxisState);
-		void final() {};
+  // init
+  const char *getUID();
+  void prepare(colorPrintfModule_t *colorPrintf_p,
+               colorPrintfModuleVA_t *colorPrintfVA_p);
 
-		//intepreter - program & lib
-        void readPC(void *buffer, unsigned int buffer_length);
-        
-        //intepreter - program
-        int startProgram(int uniq_index);
-		int endProgram(int uniq_index);
+  // compiler only
+  AxisData **getAxis(unsigned int *count_axis);
+  void *writePC(unsigned int *buffer_length);
 
-		//destructor
-		void destroy();
-		~KeyboardControlModule() {}
+  // intepreter - devices
+  int init();
+  void execute(sendAxisState_t sendAxisState);
+  void final(){};
 
-		void colorPrintf(ConsoleColor colors, const char *mask, ...);
+  // intepreter - program & lib
+  void readPC(void *buffer, unsigned int buffer_length);
+
+  // intepreter - program
+  int startProgram(int uniq_index);
+  int endProgram(int uniq_index);
+
+  // destructor
+  void destroy();
+  ~KeyboardControlModule() {}
+
+  void colorPrintf(ConsoleColor colors, const char *mask, ...);
 };
 
-#endif	/* KEYBOARD_CONTROL_MODULE_H */
+#endif /* KEYBOARD_CONTROL_MODULE_H */
